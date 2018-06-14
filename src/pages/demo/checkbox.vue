aa37f3600e970dba9c03c0c4f0e93447b14b832a
Moer <199525807@qq.com>
2018-06-14 17:36:25 +0800

    flex

--- src/pages/demo/checkbox.vue
+++ src/pages/demo/checkbox.vue
@@ -7,11 +7,10 @@
             注意由于fastclick组件影响, pc上看效果可能要双击两次鼠标
         </ui-card>
 
-        <div class="ui-content">
+        <div class="ui-content flex-column row-center">
             <ui-checkbox v-model="checked" name="inputName" label="我已阅读条款协议"></ui-checkbox>
 
-            <br>
-            是否已同意:{{ checked }}
+            <p>是否已同意:{{ checked }}</p>
         </div>
     </ui-main>
 </template>
