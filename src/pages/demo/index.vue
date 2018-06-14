cefc615954c1cc5390e3133572c66f3b6fd4faba
Moer <199525807@qq.com>
2018-06-14 17:21:04 +0800

    demo

--- src/pages/demo/index.vue
+++ src/pages/demo/index.vue
@@ -2,16 +2,16 @@
     <ui-main>
         <div class="ui-title">基础组件</div>
         <van-cell-group>
-            <van-cell title="Vant UI 组件库" is-link @click.native="link('https://youzan.github.io/vant/mobile.html#/zh-CN')"/>
-            <van-cell title="公共样式" is-link to="/demo-style" />
+            <van-cell icon="home" title="Vant UI 组件库" is-link @click.native="link('https://youzan.github.io/vant/mobile.html#/zh-CN')"/>
+            <van-cell icon="shop" title="公共样式" is-link to="/demo-style" />
         </van-cell-group>
 
         <div class="ui-title">扩展库</div>
         <van-cell-group>
             <van-cell title="子页面" is-link to="/demo-page" />
-            <van-cell title="自适应图片" is-link to="/demo-img" />
-            <van-cell title="多选框" is-link to="/demo-checkbox" />
-            <van-cell title="时间线" is-link to="/demo-timeline" />
+            <van-cell icon="photo" title="自适应图片" is-link to="/demo-img" />
+            <van-cell icon="certificate" title="多选框" is-link to="/demo-checkbox" />
+            <van-cell icon="clock" title="时间线" is-link to="/demo-timeline" />
         </van-cell-group>
     </ui-main>
 </template>
