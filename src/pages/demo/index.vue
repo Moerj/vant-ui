55caf7be289fcada6b0895785f44e60cc70979c2
Moer <199525807@qq.com>
2018-06-21 15:53:15 +0800

    demo

--- src/pages/demo/index.vue
+++ src/pages/demo/index.vue
@@ -1,9 +1,20 @@
+<style lang="scss" scoped>
+    .icon{
+        height: 16px;
+        margin-right: 5px;
+        margin-top: 5px;
+    }
+</style>
 <template>
     <ui-main>
         <div class="ui-title">基础组件</div>
         <van-cell-group>
-            <van-cell icon="home" title="Vant UI 组件库" is-link @click.native="link('https://youzan.github.io/vant/mobile.html#/zh-CN')"/>
-            <van-cell icon="shop" title="公共样式" is-link to="/demo-style" />
+            <van-cell title="Vant UI 组件库" is-link @click.native="link('https://youzan.github.io/vant/mobile.html#/zh-CN')">
+                <img class="icon" slot="icon" src="https://img.yzcdn.cn/public_files/2017/12/18/fd78cf6bb5d12e2a119d0576bedfd230.png" >
+            </van-cell>
+            <van-cell title="公共样式" is-link to="/demo-style" >
+                <img class="icon" slot="icon" src="https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2425825150,527813223&fm=58&s=8C0E7432DD276F1106E587E503005025&bpow=121&bpoh=75">
+            </van-cell>
         </van-cell-group>
 
         <div class="ui-title">扩展库</div>
@@ -15,6 +26,7 @@
             <van-cell title="加载" is-link to="/demo-loading" />
             <van-cell title="图片上传" is-link to="/demo-upload" />
             <van-cell title="下拉框" is-link to="/demo-dropdown" />
+            <van-cell title="下拉刷新/上拉加载" is-link to="/demo-pull" />
         </van-cell-group>
     </ui-main>
 </template>
