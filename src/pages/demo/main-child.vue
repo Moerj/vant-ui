33ca40e1ec5d9a07792cee2659a3a6c960278436
Moer <199525807@qq.com>
2018-07-05 19:14:56 +0800

    main demo

--- src/pages/demo/main-child.vue
+++ src/pages/demo/main-child.vue
@@ -8,6 +8,7 @@
             在路由表中用children构建子路由后,可以构建起两个ui-main之间的层级关系.
             通常用于处理页面返回后的数据和状态保持
 
+            多个ui-main之前的$loading状态是隔离的,互不影响.
             <van-button @click="testLoading" size="small">测试loading</van-button>
         </ui-card>
 
