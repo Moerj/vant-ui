1864905fff29eebe4180a85ecae73dc479bab087
Moer <199525807@qq.com>
2018-10-24 09:35:55 +0800

    ip

--- config/index.js
+++ config/index.js
@@ -3,6 +3,14 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
+let ip,activeIP=''
+try {
+    ip = require('./ip')
+    activeIP = ip[ip.active]
+    console.log(`当前ip代理: [${ip.active}]`, activeIP);
+} catch (error) {
+    console.log('尚未配置ip代理地址,请在config/中创建ip.js');
+}
 
 module.exports = {
   dev: {
@@ -10,7 +18,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+          // proxy all requests starting with /api to jsonplaceholder
+          '/api': { //跨域配置标示
+            target: `${activeIP}/api`,//所有访问标示的都被代理到这个地址,例如 http://后端域名/api
+            changeOrigin: true,
+            pathRewrite: {
+                '^/api': '/'
+            }
+        },
+    },
 
     // Various Dev Server settings
     host: '0.0.0.0', // can be overwritten by process.env.HOST
