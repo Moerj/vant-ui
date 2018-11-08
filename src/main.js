93acd4b57d6747ce34ad267dd0e3ae9ff8c2e720
Moer <199525807@qq.com>
2018-11-08 17:28:53 +0800

    update vant to 1.4.2

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ Vue.use(axios, {
 });
 
 // 主框架 vant-ui
-import 'vant/lib/vant-css/index.css';//导入全部样式
+import 'vant/lib/index.css';//导入全部样式
 // import 'vant/packages/vant-css/src/index.css'// postcss定制样式,引入所有组件目前不支持定制主题 https://github.com/youzan/vant/issues/1663
 import Vant from 'vant';
 Vue.use(Vant);
