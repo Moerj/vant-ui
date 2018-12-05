b8562a20cc4dfa9e2560ca35651399845c9f1847
Moer <199525807@qq.com>
2018-12-05 16:44:14 +0800

    安全生产基础信息

--- src/pages/企业基本信息/企业基础信息.vue
+++ src/pages/企业基本信息/企业基础信息.vue
@@ -1,31 +1,48 @@
 <template>
     <ui-main>
         <van-cell-group class="mb10">
-            <van-cell title="企业名称" value="贵州燃气(集团)有限责任公司"/>
-            <van-field label="统一社会信用代码" />
-            <van-field label="工商登记机关" />
-            <van-cell title="公司登记日期" @click="registerDateVisable=true" :value="$date(form.registerDate,'yyyy-MM-dd')">
-            </van-cell>
+            <van-cell title="企业名称" value="贵州燃气(集团)有限责任公司" />
+            <van-cell title="统一社会信用代码" />
+            <van-cell title="工商登记机关" />
+            <van-cell title="登记日期"></van-cell>
+            <van-cell title="成立日期"></van-cell>
         </van-cell-group>
-        <van-popup v-model="registerDateVisable" position="bottom" class="w-100" ref="registerDate">
-            <van-datetime-picker v-model="form.registerDate" type="date"/>
-        </van-popup>
+
+        <van-cell-group class="mb10">
+            <van-cell title="法定代言人" />
+            <van-cell title="移动电话"  />
+            <van-cell title="办公室电话"  />
+        </van-cell-group>
+
+        <van-cell-group class="mb10">
+            <van-cell title="联系人" />
+            <van-cell title="联系电话" />
+            <van-cell title="电子邮箱" />
+            <van-cell title="邮政编码" />
+        </van-cell-group>
+
+        <van-cell-group class="mb10">
+            <van-cell title="经济类型" />
+            <van-cell title="行政区域" />
+            <van-cell title="行政隶属" />
+            <van-cell title="企业状态" />
+            <van-cell title="企业安全信息管理展示 X"  value="1°2′3″"/>
+            <van-cell title="Y"  value="4°5′6″"/>
+            <van-cell title="注册地址"/>
+            <van-cell title="经营范围" label="xxx"/>
+            <van-cell title="备注" label="xxx"/>
+        </van-cell-group>
+
     </ui-main>
 </template>
 <script>
     export default {
-        data(){
+        data() {
             return {
-                form:{
-                    registerDate:''
+                form: {
                 },
-                registerDateVisable:false
             }
         },
-        methods:{
-            confirm(v){
-                this.form.registerDate=v
-            },
-        },
     }
+
 </script>
--- src/pages/企业基本信息/安全生产基础信息.vue
+++ src/pages/企业基本信息/安全生产基础信息.vue
@@ -1,30 +1,26 @@
 <template>
     <ui-main>
         <van-cell-group class="mb10">
-            <van-field required label="主要负责人" />
-            <van-field required label="主要负责人固定电话" />
-            <van-field required label="主要负责人移动电话" />
-            <van-field label="主要负责人邮箱" />
+            <van-cell title="主要负责人" />
+            <van-cell title="主要负责人固定电话" />
+            <van-cell title="主要负责人移动电话" />
+            <van-cell title="主要负责人邮箱" />
         </van-cell-group>
 
         <van-cell-group class="mb10">
-            <van-field required label="安全负责人" />
-            <van-field required label="安全负责人固定电话" />
-            <van-field required label="安全负责人移动电话" />
-            <van-field label="安全负责人邮箱" />
+            <van-cell title="安全负责人" />
+            <van-cell title="安全负责人固定电话" />
+            <van-cell title="安全负责人移动电话" />
+            <van-cell title="安全负责人邮箱" />
         </van-cell-group>
 
         <van-cell-group class="mb10">
-            <ui-picker label="安全机构设置情况" required v-model="form.security" :options="[
-                {text:'有',id:'1'},
-                {text:'无',id:'0'},
-            ]"> </ui-picker>
-            <van-field label="职业卫生负责人" />
-            <van-field label="安全监察监管机构" />
-            <van-field required label="生产\经营地址" />
+            <van-cell title="安全机构设置情况" value="有"></van-cell>
+            <van-cell title="职业卫生负责人" />
+            <van-cell title="安全监察监管机构" />
+            <van-cell title="生产\经营地址" />
         </van-cell-group>
 
-        <van-button type="primary" slot="footer" size="large">保存</van-button>
     </ui-main>
 </template>
 <script>
@@ -32,7 +28,6 @@
         data(){
             return {
                 form:{
-                    security:''
                 },
             }
         },
