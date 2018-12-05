ada8359b4d29cb7ca6b4a50137962d4b090bce68
Moer <199525807@qq.com>
2018-12-05 16:01:27 +0800

    main.js删除合并后的重复

--- src/main.js
+++ src/main.js
@@ -19,10 +19,6 @@ const router = new VueRouter({
   routes
 })
 
-// 公共方法
-import globalMethods from './assets/globalMethods'
-Vue.use(globalMethods)
-
 // http 模块
 import axios from './assets/axios';
 Vue.use(axios, {
