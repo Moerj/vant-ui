2ae88f09d7cab5d7d386af53b10e8169cdab78d5
杨圣 <199525807@qq.com>
2018-12-06 06:59:18 +0000

    撤销 "Merge branch 'dev' into 'master'"
    这将还原提交 f069b85b1d90950f0782f20b491fde532f16ebfa

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <template>
-    <div id="app" class="h-100 flex-column">
-        <div class="flex-1 relative">
-            <transition name="fade">
-                <router-view/>
-            </transition>
-        </div>
-        <bottombar></bottombar>
+    <div id="app" class="h-100">
+        <transition name="fade">
+            <router-view/>
+        </transition>
     </div>
 </template>
 
 <script>
-    import bottombar from '@/pages/bottombar'
-    
     export default {
         name: 'App',
-        components:{
-            bottombar
-        },
     }
 </script>
