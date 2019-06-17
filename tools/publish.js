14b956c6759237ac0cc3095191df4c709e6c7a9a
Moer <199525807@qq.com>
2019-06-17 16:03:36 +0800

    发布优化

--- tools/publish.js
+++ tools/publish.js
@@ -1,6 +1,14 @@
 const shell = require('shelljs');
 const path = require('path')
 const src = path.join(__dirname, '../src')
-
-// 拷贝scss样式并发布
-shell.exec(`cp -R -f ${src}/scss ${src}/vant-ui/ && cd src/vant-ui && npm publish`)
+const removeTemp = function(){
+    shell.exec(`cd src/vant-ui && rm -rf scss/`) //发布完成后删除临时文件
+}
+// 拷贝样式文件到发布目录,用于发布
+console.log('正在发布,请勿关闭进程...');
+if (shell.exec(`cp -R -f ${src}/scss ${src}/vant-ui/ && cd src/vant-ui && npm publish`).code === 0) {
+    removeTemp()
+    console.log('------------ 发布成功 ------------');
+}else{
+    removeTemp()
+}
