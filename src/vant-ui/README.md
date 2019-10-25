1de906de4b8b3a069c3f553cc9bcaa31e549412f
Moer <199525807@qq.com>
2019-10-25 11:00:16 +0800

    publish reamde sync to base readme

--- src/vant-ui/README.md
+++ src/vant-ui/README.md
@@ -38,3 +38,23 @@ import 'vant-ui/scss/base.scss'
 
 ## 浏览器支持
 现代浏览器以及 Android 4.0+, iOS 6+
+
+
+<br/><br/>
+
+## vant-ui 移动端框架
+如果你使用本脚手架搭建项目, 按以下操作
+
+### 修改组件为远程仓库
+```js
+// main.js
+import vantUI from 'vant-ui'; //npm库
+// import vantUI from './vant-ui'; //本地库
+```
+
+### 删除新项目不需要的文件
+- 删除本地组件库 src/vant-ui
+- 删除 src/pages/demo
+
+### 配置代理
+解压ip.zip,并配置你的ip地址
--- tools/publish.js
+++ tools/publish.js
@@ -6,7 +6,7 @@ const removeTemp = function(){
 }
 // 拷贝样式文件到发布目录,用于发布
 console.log('正在发布,请勿关闭进程...');
-if (shell.exec(`cp -R -f ${src}/scss ${src}/vant-ui/ && cd src/vant-ui && npm publish`).code === 0) {
+if (shell.exec(`cp README.md ${src}/vant-ui/ && cp -R -f ${src}/scss ${src}/vant-ui/ && cd src/vant-ui && npm publish`).code === 0) {
     // removeTemp()
     console.log('------------ 发布成功 ------------');
 }else{
