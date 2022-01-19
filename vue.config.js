d7604b17a880d46f44e38736d84ae6c6a9596c66
Moer <199525807@qq.com>
2022-01-19 16:42:25 +0800

    npm test

--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,7 @@ module.exports = {
                     new CompressionPlugin({
                         test: /\.js$|\.html$|\.css/, //匹配文件名
                         threshold: 10240, //对超过10k的数据进行压缩
-                        deleteOriginalAssets: false //是否删除原文件
+                        deleteOriginalAssets: false //是否删除原文件 
                     })
                 ]
             }
