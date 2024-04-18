1a6e7ef82f3fb44efc4be1c91000c7fca2f33e6e
Moer <199525807@qq.com>
2024-04-18 10:41:49 +0800

    修正lightbox图片画廊demo图片用外链导致挂掉的问题

--- src/pages/demo/lightbox.vue
+++ src/pages/demo/lightbox.vue
@@ -27,23 +27,20 @@
 
         <!-- 类似弹出层的调用方式 -->
         <ui-lightbox :items="imgList" ref="lightbox"></ui-lightbox>
+
     </ui-main>
 </template>
 <script>
     export default {
         data() {
+            let array = []
+            let i = 1
+            for (let i = 1; i <= 11; i++) {
+                array.push({src: require(`./img/lightbox/${i}.jpg`)})
+            }
+
             return {
-                imgList: [
-                    { src: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4129624088,1274782233&fm=26&gp=0.jpg', },
-                    { src: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=927747843,353555255&fm=26&gp=0.jpg', },
-                    { src: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3755841181,3217444165&fm=26&gp=0.jpg', },
-                    { src: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2754825801,773274927&fm=26&gp=0.jpg', },
-                    { src: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2761763315,1123663049&fm=26&gp=0.jpg', },
-                    { src: 'http://wx3.sinaimg.cn/large/006NJCErly1g3ofda622gj30j60ssq5x.jpg', },
-                    { src: 'http://wx2.sinaimg.cn/large/006NJCErly1g3ojogk8qnj30j60uugpj.jpg', },
-                    { src: 'http://wx2.sinaimg.cn/large/006WCzeoly1g3odc8iy6jj31400qo445.jpg', },
-                    { src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587976714688&di=6d8ec177c35e201ae5d5e0d5199632a1&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2Fb597965516a3068eb3141cfc97010d6dccf985da.jpg', },
-                ]
+                imgList: array
             }
         },
         methods: {
