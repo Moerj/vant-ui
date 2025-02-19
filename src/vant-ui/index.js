4eca660c29049ae346097d3f2f85b8a49946497e
Moer <199525807@qq.com>
2025-02-19 20:58:55 +0800

    发布2.2.4到npm仓库

--- src/vant-ui/index.js
+++ src/vant-ui/index.js
@@ -4,6 +4,7 @@ import Vant from 'vant';
 import $ from 'jqlite'
 window.$ = $
 
+// 2.2.4+更新支持vite脚手架，若遇到这里报错请升级vant-ui版本
 const components = import.meta.glob("./components/**/*.vue", { eager: true });
 
 export default {
