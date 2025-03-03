17badcec54bc1012c7d912452cb0a1db3daa74d7
Moer <199525807@qq.com>
2025-03-03 16:09:52 +0800

    更新demo首页排序

--- src/pages/demo/index.vue
+++ src/pages/demo/index.vue
@@ -7,20 +7,7 @@
 </style>
 <template>
     <ui-main>
-        <van-cell-group>
-            <van-cell title="Vant UI 组件库" is-link @click.native="link('https://youzan.github.io/vant/mobile.html#/zh-CN')">
-                <template v-slot:icon>
-                    <img class="icon" src="https://img.yzcdn.cn/public_files/2017/12/18/fd78cf6bb5d12e2a119d0576bedfd230.png" >
-                </template>
-            </van-cell>
-            <van-cell title="公共样式" is-link to="/demo-style" >
-                <template v-slot:icon>
-                    <img class="icon" src="https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2425825150,527813223&fm=58&s=8C0E7432DD276F1106E587E503005025&bpow=121&bpoh=75.jpeg">
-                </template>
-            </van-cell>
-        </van-cell-group>
-
-        <div class="ui-title">ui扩展</div>
+        <div class="ui-title">示例</div>
         <van-cell-group>
             <van-cell title="主容器/子路由" value="main" is-link to="/demo-main" />
             <van-cell title="子页面" value="page" is-link to="/demo-page" />
@@ -35,6 +22,20 @@
             <van-cell title="菜单" value="menu" is-link to="/demo-menu" />
             <van-cell title="图片画廊" value="lightbox" is-link to="/demo-lightbox" />
             <van-cell title="防水墙" value="waterwall" is-link to="/demo-waterwall" />
+        </van-cell-group>
+
+        <div class="ui-title">其他资源</div>
+        <van-cell-group>
+            <van-cell title="Vant UI 组件库" is-link @click.native="link('https://youzan.github.io/vant/mobile.html#/zh-CN')">
+                <template v-slot:icon>
+                    <img class="icon" src="https://img.yzcdn.cn/public_files/2017/12/18/fd78cf6bb5d12e2a119d0576bedfd230.png" >
+                </template>
+            </van-cell>
+            <van-cell title="公共样式" is-link to="/demo-style" >
+                <template v-slot:icon>
+                    <img class="icon" src="https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2425825150,527813223&fm=58&s=8C0E7432DD276F1106E587E503005025&bpow=121&bpoh=75.jpeg">
+                </template>
+            </van-cell>
         </van-cell-group>
     </ui-main>
 </template>
