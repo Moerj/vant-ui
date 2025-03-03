13201409bd573380c1d4207b27f45332944fd98f
Moer <199525807@qq.com>
2025-03-03 16:17:56 +0800

    更新防水墙demo

--- src/pages/demo/waterwall.vue
+++ src/pages/demo/waterwall.vue
@@ -2,11 +2,15 @@
  * @Descripttion: 防水墙
  * @Author: 杨圣
  * @Date: 2022-04-22 21:20:06
- * @LastEditors: yilong
- * @LastEditTime: 2025-03-03 14:16:40
+ * @LastEditors: 杨圣
+ * @LastEditTime: 2025-03-03 16:16:32
 -->
 <template>
     <ui-main>
+        <template v-slot:header>
+            <van-nav-bar :title="$route.path.replace('/demo-','')" left-arrow @click-left="$router.replace('/demo')"/>
+        </template>
+        
         <img @click="$refs.waterwall.open()" src="./img/cat.jpeg" style="width: 200px;position: absolute;top:50%;left:50%;transform: translate3d(-50%,-50%,0);">
         <ui-waterwall ref="waterwall" @success="onSuccess" @fail="onFail" :imgSrc="imgSrc"></ui-waterwall>
     </ui-main>
